8e545093d0553dd77bed4854d83fe132ba6aa8d1
gornis <57918465+gornis@users.noreply.github.com>
2019-11-18 22:58:23 +0300

    Add files via upload

---
+++ room3.php
@@ -0,0 +1,22 @@
+<html>
+<title>Квест</title>
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="page">
+    <div class="main">
+        <p class="room">Сколько будем сидеть?</p>
+        <ul class="variants">
+            <li><a href="room1" class="variant">Как минимум, еще пару секунд</a></li>
+            <li><a href="room2" class="variant">До обеда</a></li>
+            <li><a href="room3" class="variant">Зачем сидеть? Лучше лечь</a></li>
+            <li><a href="room4" class="variant">Да</a></li>
+        </ul>
+        <a href="/" class="getback">Я передумал</a>
+    </div>
+</div>
+</body>
+</html>
